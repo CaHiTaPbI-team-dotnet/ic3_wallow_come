ff51a2c583b52a265c39943ad3b71b0f2324c8dd
ZerAtul7 <manekrafter@gmail.com>
2020-12-15 18:35:20 +0200

    Order + Employee

--- DeliverySystem/DeliverySystem/DataModels/Order.cs
+++ DeliverySystem/DeliverySystem/DataModels/Order.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace DeliverySystem.DataModels
 {
+    [DataContract]
     class Order
     {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public List<Product> products { get; set; } = new List<Product>();
+        
     }
 }
