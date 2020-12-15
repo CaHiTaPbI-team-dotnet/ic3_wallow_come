e171cd385f7f91f8609c38b2edfcfde6b0a48dbb
BulbasDanil <64973521+BulbasDanil@users.noreply.github.com>
2020-12-15 18:50:49 +0200

    AddedCompnay

--- DeliverySystem/DeliverySystem/Repositories/Company.cs
+++ DeliverySystem/DeliverySystem/Repositories/Company.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
+using DeliverySystem.DataModels;
 
 namespace DeliverySystem.Repositories
 {
+    [DataContract]
     class Company
     {
+        public List<Employee> employees { get; set; } = new List<Employee>();
+
+        public void Display()
+        {
+            foreach (Employee e in employees)
+                Console.WriteLine(e);
+        }
     }
 }
