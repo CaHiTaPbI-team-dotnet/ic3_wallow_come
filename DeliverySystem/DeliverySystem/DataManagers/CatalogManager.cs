5d946f3f63a102d1026e3121e93f93a84bbee735
ZerAtul7 <manekrafter@gmail.com>
2020-12-16 18:25:14 +0200

    Catalog Manager(Del Product, Add Prod, Change Price)

--- DeliverySystem/DeliverySystem/DataManagers/CatalogManager.cs
+++ DeliverySystem/DeliverySystem/DataManagers/CatalogManager.cs
@@ -36,6 +36,51 @@ namespace DeliverySystem.DataManagers
                 new Product() { ID = 5, Name = "Кола", Price = 9.99F, Producer = "Кока Кола Inc." },
             });
         }
+
+
+        public void AddProduct(string productName, float price, string producer)
+        {
+
+            int lastId = Convert.ToInt32(catalog.Products.Count.ToString());
+            catalog.Products.Add(new Product { ID = lastId + 1, Name = productName, Price = price, Producer = producer });
+        }
+
+        
+        public void DelProduct(string productName)
+        {
+
+            foreach (Product p in catalog.Products)
+            {
+                if(p.Name == productName)
+                {
+                    catalog.Products.Remove(p);
+                    Console.WriteLine($"Product succesfuly removed");
+                }
+                else 
+                {
+                    Console.WriteLine($"Product with name {productName} not exists");
+                }
+            }
+            
+        }
+
+        public void ChangeProductPrice(string productName,float price)
+        {
+
+            foreach (Product p in catalog.Products)
+            {
+                if (p.Name == productName)
+                {
+                    p.Price = price;
+                    Console.WriteLine($"Product price succesfuly changed");
+                }
+                else
+                {
+                    Console.WriteLine($"Product with name {productName} not exists");
+                }
+            }
+
+        }
         public void Display()
         {
             Console.WriteLine(catalog);
