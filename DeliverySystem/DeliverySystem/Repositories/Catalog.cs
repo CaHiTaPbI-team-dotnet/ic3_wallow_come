37c26e658d27f5fb226774ca0684874e4c5aa7d4
ZerAtul7 <manekrafter@gmail.com>
2020-12-16 17:03:20 +0200

    Merge with daniil branch

--- DeliverySystem/DeliverySystem/Repositories/Catalog.cs
+++ DeliverySystem/DeliverySystem/Repositories/Catalog.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Runtime.Serialization;
+using DeliverySystem.DataModels;
 namespace DeliverySystem.Repositories
 {
+    [DataContract]
     class Catalog
     {
+        [DataMember]
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public override string ToString()
+        {
+            string res = "";
+            foreach(Product p in Products)
+            {
+                res += $"  {p.ID}. {p.Name}  \"{p.Producer}\"  -> {p.Price} \n";
+            }
+            return res;
+        }
     }
 }
