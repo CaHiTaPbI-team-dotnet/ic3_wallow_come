3819aa9b424ba4209c309359d2451d5150156a8c
Danya Boisan <boberiosif@gmail.com>
2020-12-16 17:27:56 +0200

    123
    123

--- DeliverySystem/DeliverySystem/DataModels/Order.cs
+++ DeliverySystem/DeliverySystem/DataModels/Order.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.Serialization;
 
 namespace DeliverySystem.DataModels
 {
+    [DataContract]
     class Order
     {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public List<Product> products { get; set; } = new List<Product>();
+        [DataMember]
+        public string BuyerFName { get; set; }
+        
     }
 }
